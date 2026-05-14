df158aa15a15a56d219c098fa08e4a47d9b93c6c
Tu <Tu@192.168.0.15>
2026-05-15 01:41:21 +0900

    Add student form and POST feature

--- student-management/src/main/java/com/example/demo/StudentController.java
+++ student-management/src/main/java/com/example/demo/StudentController.java
@@ -1,13 +1,42 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-	public class StudentController{
-		@GetMapping("/students")
-		public String listStudents() {
-			return "students";
-		}
-	}
+import com.example.demo.model.Student;
 
+@Controller
+public class StudentController {
+
+    @GetMapping("/students")
+    public String showStudents(Model model) {
+
+        List<Student> students = new ArrayList<>();
+
+        students.add(new Student(1, "minh anh", 20, "minhanh@gmail.com"));
+        students.add(new Student(2, "phong", 21, "phong@gmail.com"));
+        students.add(new Student(3, "nam", 22, "nam@gmail.com"));
+
+        model.addAttribute("students", students);
+
+        return "students";
+    }
+    @GetMapping("/add-student")
+    public String addStudentForm() {
+    	
+        return "add-student";
+    }
+    @PostMapping("/students/add")
+    public String addStudent(Student student) {
+
+        System.out.println(student.getName());
+        System.out.println(student.getAge());
+        System.out.println(student.getEmail());
+
+        return "redirect:/students";
+    }
+}
