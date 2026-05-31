2c3092075f8c93d31bbb1561b6001cb5887ced5a
Tu <Tu@192.168.0.15>
2026-06-01 00:39:12 +0900

    Add student list and auto increment ID

--- student-management/src/main/java/com/example/demo/StudentController.java
+++ student-management/src/main/java/com/example/demo/StudentController.java
@@ -2,40 +2,48 @@ package com.example.demo;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.model.Student;
 
 @Controller
 public class StudentController {
 
-    @GetMapping("/students")
-    public String showStudents(Model model) {
-
-        List<Student> students = new ArrayList<>();
+    private List<Student> students = new ArrayList<>();
+    private int nextId = 4;
+    public StudentController() {
 
         students.add(new Student(1, "minh anh", 20, "minhanh@gmail.com"));
         students.add(new Student(2, "phong", 21, "phong@gmail.com"));
         students.add(new Student(3, "nam", 22, "nam@gmail.com"));
 
+    }
+
+    @GetMapping("/students")
+    public String showStudents(Model model) {
+
         model.addAttribute("students", students);
 
         return "students";
     }
+
     @GetMapping("/add-student")
     public String addStudentForm() {
-    	
+
         return "add-student";
     }
+
     @PostMapping("/students/add")
     public String addStudent(Student student) {
 
-        System.out.println(student.getName());
-        System.out.println(student.getAge());
-        System.out.println(student.getEmail());
+        student.setId(nextId);
+        nextId++;
+
+        students.add(student);
 
         return "redirect:/students";
     }
